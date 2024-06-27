e235356383a06b54007b8714963a4e6f1fad44d0
Gopal Krushna Panda <gopalpanda2002@gmail.com>
2024-06-27 11:59:05 +0530

    the Page Content Diplay section completed

--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index',{
-        heading:"supermall"
-    });
+//Get Page model
+const page = require("../models/page");
+
+
+//Get Home Page
+router.get('/', async (req, res) => {
+    var slug = "home";
+    try{
+        var p = await page.findOne({slug: slug});
+        res.render('index',{
+            heading:"supermall",
+            title:  p.title,
+            content: p.content,
+        });
+    }catch(err){
+        return console.log("err :", err);
+    }
+    
+});
+
+//Get a Page
+router.get('/:slug', async (req, res) => {
+    var slug = req.params.slug;
+    try{
+        var p = await page.findOne({slug: slug});
+        if(!p){
+            console.log("null page");
+            res.redirect("/");
+        }else{
+            res.render('index',{
+                heading:"supermall",
+                title:  p.title,
+                content: p.content,
+            });
+        }
+    }catch(err){
+        return console.log("err :", err);
+    }
+    
 });
 
 
